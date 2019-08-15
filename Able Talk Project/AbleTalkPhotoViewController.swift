312faa572e040f42c0ad771e26faea663e4db777
Alencia <alencia@Apples-MacBook-Pro-18.local>
2019-08-15 12:41:56 -0700

    update

---
+++ Able Talk Project/AbleTalkPhotoViewController.swift
@@ -0,0 +1,53 @@
+//
+//  AddPhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Mary Lee
+// * based off of Sofia Ongele
+//
+import UIKit
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func saveButton(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = textField.text
+            if let userImage = imageView.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            navigationController?.popViewController(animated: true)
+        }
+        
+    }
+
+
+
+
+    @IBAction func cameraButton(_ sender: Any) {
+        imagePicker.sourceType = .camera; present(imagePicker, animated: true, completion: nil)
+    
+}
+    
+@IBAction func albumButton(_ sender: Any) {
+    imagePicker.sourceType = .photoLibrary
+    present(imagePicker, animated: true, completion: nil)
+}
+
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Able Talk Project/AbleTalkTableTableViewController.swift
@@ -0,0 +1,93 @@
+//  PhotoTableViewController.swift
+//  ViewFinder
+//
+//  Created by Mary Lee
+// * based off of Sofia Ongele
+//
+import UIKit
+class PhotoTableViewController: UITableViewController {
+    
+    var photos : [Photos] = []
+    
+    func getPhotos() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            // now we need to search through Core Data to find our photos/captions
+            
+    if let coreDataPhotos = try? context.fetch(Photos.fetchRequest()) as? [Photos] {
+                photos = coreDataPhotos
+                tableView.reloadData()
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+  
+    override func viewWillAppear(_ animated: Bool) {
+        getPhotos()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return photos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let cellPhoto = photos[indexPath.row]
+        
+        cell.textLabel?.text = cellPhoto.caption
+        
+        if let cellPhotoImageData = cellPhoto.imageData {
+            if let cellPhotoImage = UIImage(data: cellPhotoImageData) {
+                cell.imageView?.image = cellPhotoImage
+            }
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveToDetail", sender: photos[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToDetail" {
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+    
+                if let photoToSend = sender as? Photos {
+                    photoDetailView.photo = photoToSend
+                }
+                
+            }
+        }
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let photoToDelete = photos[indexPath.row]
+                context.delete(photoToDelete)
+                (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                getPhotos()
+                //save to context, then reload
+            }
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+        
+    }
+}
